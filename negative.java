a68a8734bd3009db219c1fe601f624352072921f
kavinkumar06 <kavinm09@gmail.com>
2017-07-10 11:39:41 +0530

    Create negative.java

---
+++ negative.java
@@ -0,0 +1,18 @@
+# negative
+import java.util.*;
+public class negative
+{
+    public static void main(String args[])
+    {
+      int num;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter any integer number:");
+      num=sc.nextInt();
+      if(num>0)
+          System.out.println(num+"is POSITIVE NUMBER");
+      else if(num<0)
+          System.out.println(num+"is NEGATIVE NUMBER");
+      else
+          System.out.println("it is ZERO");
+          }
+      }    
